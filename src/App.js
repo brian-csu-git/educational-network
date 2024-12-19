80647ea06df49fb250eafd64fa3ac32c7a562a38
Brian Kelly <brianpk@colostate.edu>
2024-12-19 15:55:31 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import NetworkVisualization from './components/NetworkVisualization';
+
+function App() {
+  return (
+    <div className="App">
+      <NetworkVisualization />
+    </div>
+  );
+}
+
+export default App;
